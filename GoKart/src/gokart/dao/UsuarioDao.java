4b5688431be4ce9eef887fb0a4e8e0d8a67b2286
pirees <leonardopires-1997@hotmail.com>
2020-02-22 14:40:34 -0300

    Criando package e classes (Dao e Teste)

---
+++ GoKart/src/gokart/dao/UsuarioDao.java
@@ -0,0 +1,5 @@
+package gokart.dao;
+
+public class UsuarioDao {
+
+}
---
+++ GoKart/src/gokart/teste/UsuarioTeste.java
@@ -0,0 +1,10 @@
+package gokart.teste;
+
+public class UsuarioTeste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
